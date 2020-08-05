622dfa4abd433b496e4b65c62cf992f81b5bd7ef
World <1304011993@qq.com>
2020-08-05 17:01:03 +0800

    basic  knowledge

--- Day1.py
+++ Day1.py
@@ -19,14 +19,16 @@ classNum = ['1st', '2nd', '3rd']
 tuple1 = ('1', 2, '3')
 
 #conditional judgment
-s = input('birth: ')
-birth = int(s)
-if birth < 2000:
-    print('<00')
-else:
-    print('>00')
+# s = input('birth: ')
+# birth = int(s)
+# if birth < 2000:
+#     print('<00')
+# else:
+#     print('>00')
 
 #loop   for...in / while
 #dict and set
-d = {'bsr': 1, 'a': 2, 'c': 3}
-print(d)
+d = {'a': 1, 'b': 2, 'c': 3}
+
+#function    默认参数必须指向不变对象！     *可变参数       **关键字参数
+#和关键字参数**kw不同，命名关键字参数需要一个特殊分隔符*，*后面的参数被视为命名关键字参数。
